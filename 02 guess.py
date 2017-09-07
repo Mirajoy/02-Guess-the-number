fb1150bc415c83314d66cca1ab821fd44273ed09
Mirajoy <31677653+Mirajoy@users.noreply.github.com>
2017-09-06 21:31:57 -0500

    Update 02 guess.py

--- 02 guess.py
+++ 02 guess.py
@@ -5,27 +5,33 @@ import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
 
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
+
 number = random.randint(1,20)
 
+print("Hello, and welcome to the game! You need to guess a number between 1 and 20 to win! But be careful, you only get 5 tries! Good luck!")
+
+try_num = 5
+
+while try_num > 0:
+	guess = input()
+	try_num += -1
 
 
-#ask the user for a response and store it in the variable [guess]
-guess = input()
+	try:
+		#convert the guess to an integer
+		guess = int(guess)
 
 
-
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
-
-
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
+		#check if the guess is less than the random number
+		if guess < number:
+			print('Too low! You have ' +try_num + ' guesses left!')
+		if guess > number:
+			print('Too high! You have ' +try_num +' guesses left!')
+		if guess ==number:
+			print('Congratulations! You guessed right!')
 
 
-except ValueError:
-	print('Please enter a whole number.')
+	except ValueError:
+		print('Please enter a whole number.')
+print("Would you like to try again?")
 
